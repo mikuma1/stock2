c38a5cd7893d14191bea776c77a140bc110516a0
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2024-12-25 02:18:49 +0900

    fix: rswagの初期設定を修正
    - swagger_rootの設定を追加
    - SwaggerUIの基本設定を修正

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,8 +1,3 @@
 Rswag::Ui.configure do |c|
   c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
-
-  c.config_object[:defaultModelsExpandDepth] = 0
-  c.config_object[:displayRequestDuration] = true
-  c.config_object[:docExpansion] = 'list'
-  c.config_object[:persistAuthorization] = true
 end
