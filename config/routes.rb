b4ab4e331d0eaece9a2fcf38103c039670ff3bbc
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2024-12-24 23:20:18 +0900

    feat: API用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :companies, only: [:index, :show, :create, :update] do
+        resources :users, only: [:index, :show, :create, :update]
+      end
+    end
+  end
 end
