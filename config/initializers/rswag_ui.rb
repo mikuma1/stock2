f436b6241411545b3f3dd5678469a2c96e96f93b
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2024-12-25 03:08:49 +0900

    feat: OpenAPI仕様のドキュメントを追加
    - rswag-api, rswag-uiの設定を追加
    - Companies APIのドキュメントを追加
    - Users APIのドキュメントを追加
    - 日本語化対応
    - エラーレスポンスの定義を統一化

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,1 +1,22 @@
- 
+Rswag::Ui.configure do |c|
+  c.openapi_endpoint 'api-docs/v1/swagger.yaml', 'API V1 Docs'
+
+  # UIのカスタマイズ
+  c.basic_auth_enabled = false
+
+  # 設定オブジェクト
+  c.config_object = {
+    url: '/api-docs/v1/swagger.yaml',
+    docExpansion: 'list',
+    deepLinking: true,
+    defaultModelsExpandDepth: 3,
+    defaultModelExpandDepth: 3,
+    displayOperationId: false,
+    displayRequestDuration: true,
+    filter: true,
+    showExtensions: true,
+    showCommonExtensions: true,
+    tryItOutEnabled: true,
+    language: 'ja'
+  }
+end
