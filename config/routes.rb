7d24fb83eb33c0a1ec64d75eca567e671712528a
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2024-12-25 02:39:47 +0900

    chore: APIドキュメントのルーティングを整理
    - APIドキュメントのマウントポイントを追加
    - コメントを追加してルーティングの目的を明確化

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
-  
+
   namespace :api do
     namespace :v1 do
       resources :companies, only: [:index, :show, :create, :update] do
@@ -10,4 +8,7 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 end
