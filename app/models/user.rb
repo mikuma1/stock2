d31210bd609b17ae40d06501ae06e14173ba333e
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2024-12-26 00:06:33 +0900

    feat: Userモデルにadmin?メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   validates :role, presence: true
   validates :department, presence: true, if: :require_department?
 
+  def admin?
+    role == 'admin'
+  end
+
   private
 
   def require_department?
