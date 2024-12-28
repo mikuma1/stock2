19d0f200658720595e524ad628844f4b1e691007
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2024-12-29 02:26:52 +0900

    feat: 消耗品名と会社IDの組み合わせにユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_28_155734) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_28_170243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_28_155734) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["company_id"], name: "index_items_on_company_id"
+    t.index ["name", "company_id"], name: "index_items_on_name_and_company_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
