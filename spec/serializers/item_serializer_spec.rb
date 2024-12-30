8ac8bb535c58ac9dc29dc3ac3e9e06644505e981
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2024-12-31 01:20:14 +0900

    ItemSerializerのテストを改善
    - 属性テストを論理的なグループに分割
    - 期待値の数を適切な量に調整
    - RuboCopの指摘（RSpec/MultipleExpectations）に対応

--- spec/serializers/item_serializer_spec.rb
+++ spec/serializers/item_serializer_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ItemSerializer, type: :serializer do
-  let(:item) { create(:item) }
+  let(:company) { create(:company) }
+  let(:category) { create(:category, company: company) }
+  let(:item) { create(:item, company: company, category: category) }
   let(:serializer) { described_class.new(item) }
   let(:serialization) { JSON.parse(serializer.to_json) }
 
   it '基本的な属性がシリアライズされること' do
     expect(serialization.keys).to include(
-      'id', 'name', 'description', 'minimum_quantity', 'current_stock'
+      'id', 'name', 'description', 'minimum_quantity', 'stock_quantity'
     )
   end
 
@@ -27,5 +29,23 @@ RSpec.describe ItemSerializer, type: :serializer do
     expect(serialization.keys).to include(
       'category', 'company', 'stocks'
     )
+  end
+
+  describe '属性の値' do
+    it '基本情報が正しくシリアライズされること' do
+      expect(serialization['name']).to eq(item.name)
+      expect(serialization['stock_quantity']).to eq(item.stock_quantity)
+    end
+
+    it '関連IDが正しくシリアライズされること' do
+      expect(serialization['category_id']).to eq(item.category_id)
+      expect(serialization['company_id']).to eq(item.company_id)
+    end
+  end
+
+  it '関連モデルが正しくシリアライズされること' do
+    expect(serialization['category']).to be_present
+    expect(serialization['company']).to be_present
+    expect(serialization['stocks']).to eq([])
   end
 end
